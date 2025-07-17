a8cc566b881820c41fad25d5fb1979ba1e94d9b4
admin <admin@MacBook-Air-cua-admin.local>
2025-07-17 11:55:31 +0700

    validate user form

--- src/controllers/client/product.controller.ts
+++ src/controllers/client/product.controller.ts
@@ -1,19 +1,42 @@
+import { error } from "console";
 import { Request, Response } from "express";
 import { ProductSchema, TProductSchema } from "src/validation/user.schema";
 
 const getAdminCreateProduct = (req: Request, res: Response) => {
-    return res.render("admin/product/create.ejs")
+    const errors:any = []
+    const oldData = {
+        name:"",
+        price: "",
+        detailDesc: "",
+        shortDesc: "",
+        factory: "",
+        target:""
+    }
+    return res.render("admin/product/create.ejs",{errors, oldData})
 }
 
 const postAdminCreateProduct = (req: Request, res: Response) => {
     const {name} = req.body as TProductSchema
-    try {
-        const result = ProductSchema.parse(req.body)
-        console.log("run ok", result);
-    } catch (error) {
-        // console.log(error)
+
+    const validate = ProductSchema.safeParse(req.body)
+    if(!validate.success){
+        //error
+        const errorsZod = validate.error.issues;
+        const errors = errorsZod.map((item: any)=>`${item.message} (${item.path[0]})`)
+        const oldData = {
+        name:"",
+        price: "",
+        detailDesc: "",
+        shortDesc: "",
+        factory: "",
+        target:""
     }
-    res.redirect("/admin/product")
+    return res.render("admin/product/create.ejs", {errors, oldData})
+        
+    }
+    //success
+    return res.render("admin/product/create.ejs")
+
 }
 
 export {getAdminCreateProduct, postAdminCreateProduct}
--- src/validation/user.schema.ts
+++ src/validation/user.schema.ts
@@ -1,23 +1,26 @@
+import { z } from "zod"
 
-import { number, z } from "zod"
-
-export const ProductSchema = z.object(
-    {
-        name: z.string().trim().min(1, {message: "Tên không được để trống"}),
-        price: z.number().transform((val: any)=>(val === "" ? 0 : Number(val))).refine,
-        detailDesc: z.string().trim().min(1, {message: "Detail description không được để trống"}),
-        shortDesc: z.string().trim().min(1, {message: "Short description không được để trống"}),
-        quantity: z.number().min(1, {message: "Số lượng tối thiểu là 1"}),
-        factory: z.string().trim().min(1, {message: "Factory không được để trống"}),
-        target: z.string().trim().min(1, {message: "Target không được để trống"})
-        // image      String? @db.VarChar(255)
-        // detailDesc String  @db.VarChar(255)
-        // shortDesc  String  @db.VarChar(255)
-        // factory    String  @db.VarChar(255)
-        // quantity   Int
-        // target     String  @db.VarChar(255)
-        // sold       Int     @default(0)
-    }
-)
+export const ProductSchema = z.object({
+    name: z.string().trim().min(1, {message: "Tên không được để trống"}),
+    price: z.preprocess(
+        (val) => typeof val === "string" ? Number(val) : val,
+        z.number().refine((num) => num > 0, {message: "Số tiền tối thiểu là 1"})
+    ),
+    detailDesc: z.string().trim().min(1, {message: "Detail description không được để trống"}),
+    shortDesc: z.string().trim().min(1, {message: "Short description không được để trống"}),
+    quantity: z.preprocess(
+        (val) => typeof val === "string" ? Number(val) : val,
+        z.number().refine((num) => num > 0, {message: "Số lượng tối thiểu là 1"})
+    ),
+    factory: z.string().trim().min(1, {message: "Factory không được để trống"}),
+    target: z.string().trim().min(1, {message: "Target không được để trống"})
+    // image      String? @db.VarChar(255)
+    // detailDesc String  @db.VarChar(255)
+    // shortDesc  String  @db.VarChar(255)
+    // factory    String  @db.VarChar(255)
+    // quantity   Int
+    // target     String  @db.VarChar(255)
+    // sold       Int     @default(0)
+})
 
 export type TProductSchema = z.infer<typeof ProductSchema>;
